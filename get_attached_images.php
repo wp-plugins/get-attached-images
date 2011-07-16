9ee608bf4f5625a6e339bf9ee3543ea12746f620
solidhex <solidhex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-16 02:24:29 +0000

    adding get-attached-images to WP plugin repo
    git-svn-id: https://plugins.svn.wordpress.org/get-attached-images/trunk@410522 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ get_attached_images.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Plugin Name: Get Attached Images
+Plugin URI: http://solidhex.com/get-attached-images
+Description: Grabs all attached images from a post or page. Compatible with NivoSlider and jCarousel jQuery plugins.
+Version: 1.0
+Author: patrick@solidhex.com
+Author URI: http://solidhex.com
+
+/*  Copyright 2011  Patrick Jackson  (email : patrick@solidhex.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+function get_attached_images($pageid = FALSE, $size = "thumbnail", $single = FALSE, $prepend = "", $append = "" )
+{
+	$output = "";
+
+	// first check if we've passed in a specific page ID
+	if ($pageid != FALSE) {
+		$id = $pageid;
+	} else {
+		global $post;
+		$id = $post->ID;
+	}
+	
+	// now, retrieve all the images
+	$images = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID'));
+
+
+	if ($images) {
+		foreach ($images as $image) {
+			$i++;
+			
+			if ($image->post_excerpt) {
+				$caption = '<span class="get_attached_caption">' . $image->post_excerpt . '</span>'; 
+			} else {
+				$caption = '';
+			}
+			
+			// this is a bit of a hack to see if a link is in the description area
+			if ($link = $image->post_content) {
+				$output .= "{$prepend}<a href='$link' rel='external'>" . wp_get_attachment_image($image->ID, $size)  . "</a>{$caption}{$append}";
+			} else {
+				$output .= $prepend . wp_get_attachment_image($image->ID, $size) . $caption . $append;
+			}
+			if ($single == TRUE && $i == 1)  break;
+		}
+
+		return $output;
+
+	} else {
+		return false;
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,28 @@
+Get Attached Images pulls all attached images from a post or page. To use it, simply echo out the function either inside or out side of the loop. 
+
+<?php echo get_attached_images($pageid = FALSE, $size = "thumbnail", $single = FALSE, $prepend = "", $append = "" ); ?>
+
+$pageid: Use this if you are not pulling images from either the current page or post, or just would like to pull from some other page in general. Defaults to FALSE.
+
+$size: Select "thumbnail", "medium" or "large." Defaults to large.
+
+$single: If TRUE, this pulls the first image, if FALSE it pulls all images attached. Defaults to FALSE.
+
+$prepend: HTML tag to prepend to the image. 
+
+$append: HTML tag to append to the image.
+
+Additional Notes:
+
+Wrap in the Image in a Link
+-----------------------------------------------------------
+It's a bit of a hack, but if you enter in a value for Description in the image view, this function will wrap it in a link. For example, Description => http://google.com will create:
+<a href='http:///google.com' rel='external'>Description Content</a>
+
+Image Caption
+-----------------------------------------------------------
+If a caption is entered, this function will automatically append it to the photo, wrapped in a span for you to style as you wish:
+
+<img src="foo.gif" /><span class="get_attached_caption">My Crazy Caption</span>
+
+That's it: ENJOY!
